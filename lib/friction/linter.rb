d10abc0e9288638009d181868f08442bae0692e3
Rafal Chmiel <rafalmarekchmiel@gmail.com>
2014-03-24 17:47:24 +0000

    Add explanations to errors

--- lib/friction/linter.rb
+++ lib/friction/linter.rb
@@ -3,37 +3,39 @@ module Friction
     # Checks/logs if README exists
     # @return [nil,true] nil if file exists, true if not
     def check_readme
-      log 'README not found.' unless check? 'README*'
+      log 'README not found', 'yHosNA' unless check? 'README*'
     end
 
     # Checks/logs if CONTRIBUTING guide exists
     # @return [nil,true] nil if file exists, true if not
     def check_contributing
-      log 'CONTRIBUTING guide not found.' unless check? 'CONTRIBUTING*'
+      log 'CONTRIBUTING guide not found', 'g_0mVQ' unless check? 'CONTRIBUTING*'
     end
 
     # Checks/logs if LICENSE exists
     # @return [nil,true] nil if file exists, true if not
     def check_license
-      log 'LICENSE not found.' unless check? 'LICENSE*'
+      log 'LICENSE not found', 'pFMQMQ' unless check? 'LICENSE*'
     end
 
     # Checks/logs if bootstrap script exists
     # @return [nil,true] nil if file exists, true if not
     def check_bootstrap
-      log 'Bootstrap script not found.' unless check? 'script/bootstrap'
+      log 'Bootstrap script not found', 'jZoRYA' unless check? 'script/bootstrap'
     end
 
     # Checks/logs if test script exists
     # @return [nil,true] nil if file exists, true if not
     def check_test
-      log 'Test script not found.' unless check? 'script/test'
+      log 'Test script not found', 'oo21Jw' unless check? 'script/test'
     end
 
     # Checks/logs if .gitignore exists (if directory is Git repository)
     # @return [nil,true] nil if file exists, true if not
     def check_gitignore
-      (log '.gitignore not found.' unless check? '.gitignore') if check? '.git'
+      if check? '.git'
+        log '.gitignore not found', 'pevJkA' unless check? '.gitignore'
+      end
     end
 
     private
@@ -42,8 +44,8 @@ module Friction
       !Dir.glob(filename).empty?
     end
 
-    def log(message)
-      puts "[#{'ERROR'.red}] #{message.blue}"
+    def log(message, source)
+      puts "[#{'ERROR'.red}] #{message.blue} (see http://git.io/#{source})"
       true
     end
   end
